b40cfaae4b6a459a8760f15f166edcf267a0e153
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-17 14:30:45 +0000

    monty

---
+++ monty.c
@@ -0,0 +1,87 @@
+#include "monty.h"
+
+/* global flag struct for queue and stack length */
+var_t var;
+
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
+	unsigned int line = 0;
+	FILE *myfile = NULL;
+	char *lineptr = NULL, *oper = NULL;
+	size_t llen = 0;
+
+	var.len_queue = 0;
+	var.len_stack = 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	myfile = fopen(argv[1], "r");
+	if (myfile == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	on_exit(free_lineptr, &lineptr);
+	on_exit(free_stack, &stack);
+	on_exit(myfile_close, myfile);
+
+	while (getline(&lineptr, &llen, myfile) != -1)
+	{
+		line++;
+		oper = strtok(lineptr, "\n\t\r ");
+		if (oper != NULL && oper[0] != '#')
+		{
+			call_oper(&stack, oper, line);
+		}
+	}
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * free_lineptr - free line pointer returned by getline
+ * @status: exit status
+ * @arg: pointer to line
+ */
+void free_lineptr(int status, void *arg)
+{
+	char **lineptr = arg;
+	(void) status;
+	if (*lineptr != NULL)
+		free(*lineptr);
+}
+
+/**
+ * myfile_close - close file stream
+ * @status: status passed to exit
+ * @arg: pointer to file stream
+ */
+void myfile_close(int status, void *arg)
+{
+	FILE *myfile;
+	(void) status;
+	myfile = (FILE *) arg;
+	fclose(myfile);
+}
+
+/**
+ * check_isdigit - checks string is only digit
+ * @str: string input argument
+ * Return: 0 is only digit or 1 not digit
+ */
+
+int check_isdigit(char *str)
+{
+	int c;
+	for (c = 0; str[c]; c++)
+	{
+		if (str[c] == '-' && c == 0)
+			continue;
+		if (isdigit(str[c]) == 0)
+			return (1);
+	}
+	return (0);
+}
---
+++ stack.c
@@ -0,0 +1,90 @@
+#include "monty.h"
+
+/**
+ * add_node - add a new node to a circular linked list
+ * @stack: double pointer to the beginning of the circular linked list
+ * @n: value to add to the new node
+ *
+ * Description: the function will add the node to the begining if in
+ * stack mode and the end if in queue mode
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+stack_t *add_node(stack_t **stack, const int n)
+{
+	stack_t *new;
+
+	if (stack == NULL)
+		return (NULL);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*stack == NULL)
+	{
+		new->prev = new;
+		new->next = new;
+	}
+	else
+	{
+		(*stack)->prev->next = new;
+		new->prev = (*stack)->prev;
+		(*stack)->prev = new;
+		new->next = *stack;
+	}
+	if (var.len_queue == STACK || var.len_stack == 0)
+		*stack = new;
+	return (new);
+}
+
+/* global struct to hold flag for queue and stack length */
+var_t var;
+
+/**
+ * free_stack - frees malloced in stack nodes memory
+ * @arg: double pointer to the stack
+ * @status: exit status
+ */
+void free_stack(int status, void *arg)
+{
+	stack_t **stack;
+	stack_t *tmp;
+	(void) status;
+	stack = (stack_t **)arg;
+	if (*stack)
+	{
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
+	while (*stack != NULL)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+	var.len_stack = 0;
+}
+
+/**
+ * set_queue - sets the format of the data to a queue (FIFO)
+ * @stack: double pointer to the top of the stack
+ * @line: script line number
+ */
+void set_queue(stack_t **stack, unsigned int line)
+{
+	(void) stack;
+	(void) line;
+	var.len_queue = QUEUE;
+}
+
+/**
+ * set_stack - sets the format of the data to a stack (LIFO)
+ * @stack: double pointer to the top of the stack
+ * @line: script line number
+ */
+void set_stack(stack_t **stack, unsigned int line)
+{
+	(void) stack;
+	(void) line;
+	var.len_queue = STACK;
+}
